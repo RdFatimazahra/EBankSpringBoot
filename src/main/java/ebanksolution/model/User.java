98d9e714621dd660a54cf458f2cdd54733303f9e
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-07-03 10:21:12 +0100

    Création de la classe User

---
+++ src/main/java/ebanksolution/model/User.java
@@ -0,0 +1,22 @@
+package ebanksolution.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int idUser ;
+    private String name;
+    private String email;
+    private String password;
+}
