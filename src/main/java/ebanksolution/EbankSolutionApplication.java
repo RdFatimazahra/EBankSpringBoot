7a5cf5aff4006166cac821906189f9b7ff1f21a1
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-07-11 10:01:19 +0100

    implements of user class from userDetails Class and create enum role

--- src/main/java/ebanksolution/EbankSolutionApplication.java
+++ src/main/java/ebanksolution/EbankSolutionApplication.java
@@ -2,6 +2,12 @@ package ebanksolution;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.net.PasswordAuthentication;
 
 @SpringBootApplication
 public class EbankSolutionApplication {
@@ -10,4 +16,8 @@ public class EbankSolutionApplication {
         SpringApplication.run(EbankSolutionApplication.class, args);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
---
+++ src/main/java/ebanksolution/controller/HomeController.java
@@ -0,0 +1,13 @@
+package ebanksolution.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String HomeController() {
+        return "Welcome to Spring Security";
+    }
+
+}
