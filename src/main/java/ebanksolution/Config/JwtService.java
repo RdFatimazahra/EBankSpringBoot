b2f79552dd1f0883ced07c5359e0c28e0b45da27
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-07-11 14:23:20 +0100

    GetSigning Method in JwtsService class

--- src/main/java/ebanksolution/Config/JwtService.java
+++ src/main/java/ebanksolution/Config/JwtService.java
@@ -2,12 +2,18 @@ package ebanksolution.Config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Base64;
 
 @Service
 public class JwtService {
 
-    private static final  String SECRET_KEY ="";
+    private static final  String SECRET_KEY ="OQ8s4uDjTJqQNtghPJEsQ6o75iX00vYuT88EMpCiJIA=\n";
 
     public String extractUsername(String token) {
         return null;
@@ -20,5 +26,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
